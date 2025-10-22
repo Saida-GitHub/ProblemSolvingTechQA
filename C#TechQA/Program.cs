ce24f9edd0af6031d480786e82447c0e204021e6
Saida Shaik <saidashaik6293@gmail.com>
2025-10-22 21:17:27 +0530

    Recap of Reverse String

--- C#TechQA/Program.cs
+++ C#TechQA/Program.cs
@@ -2,6 +2,26 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Reverse String ::
+        Console.WriteLine("Enter a string to reverse:");
+
+        /*John  lenghth = 4-1 = 3
+         3 2 1 0
+         nhoJ
+         J o h n
+         0 1 2 3
+         
+         0 < 4
+         1 < 4
+         2 < 4
+         3 < 4
+         
+         i = 3; i >=0; i--
+            */
+        string input = Console.ReadLine();
+        for (int i = input.Length - 1; i >= 0; i--)
+        {
+            Console.Write(input[i]);
+        }
     }
 }
